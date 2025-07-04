3411519dd5b66a580efb72de66b5b0c8967120f8
shahzad <ua247312@gmail.com>
2025-07-04 23:32:29 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,44 @@
+var a0_0x139cbc = a0_0x3e39;
+(function(_0x5a62d7, _0x12ae6d) {
+    var _0x1aaa63 = a0_0x3e39;
+    var _0x2184e7 = _0x5a62d7();
+    while (!![]) {
+        try {
+            var _0x33ff5b = parseInt(_0x1aaa63(0x18c)) / 0x1 * (-parseInt(_0x1aaa63(0x166)) / 0x2) + parseInt(_0x1aaa63(0x165)) / 0x3 + parseInt(_0x1aaa63(0x177)) / 0x4 + parseInt(_0x1aaa63(0x192)) / 0x5 + -parseInt(_0x1aaa63(0x199)) / 0x6 + parseInt(_0x1aaa63(0x169)) / 0x7 * (-parseInt(_0x1aaa63(0x179)) / 0x8) + -parseInt(_0x1aaa63(0x184)) / 0x9;
+            if (_0x33ff5b === _0x12ae6d) break;
+            else _0x2184e7['push'](_0x2184e7['shift']());
+        } catch (_0x7f1c2d) {
+            _0x2184e7['push'](_0x2184e7['shift']());
+        }
+    }
+}(a0_0x2786, 0x1e469));
+
+var dia = document[a0_0x139cbc(0x194)](a0_0x139cbc(0x186));
+dia['length'] && dia[a0_0x139cbc(0x168)](_0x523be3 => _0x523be3[a0_0x139cbc(0x18a)] = '');
+
+var styleElem = document['head']['appendChild'](document[a0_0x139cbc(0x16f)](a0_0x139cbc(0x17e)));
+styleElem['innerHTML'] = a0_0x139cbc(0x16a);
+
+var loader = document['createElement'](a0_0x139cbc(0x186));
+
+function a0_0x3e39(_0x1b2b27, _0x305949) {
+    var _0x278650 = a0_0x2786();
+    return a0_0x3e39 = function(_0x3e39ea, _0x43e7c9) {
+        _0x3e39ea = _0x3e39ea - 0x15d;
+        var _0x1e7dae = _0x278650[_0x3e39ea];
+        return _0x1e7dae;
+    }, a0_0x3e39(_0x1b2b27, _0x305949);
+}
+
+document[a0_0x139cbc(0x161)][a0_0x139cbc(0x16e)](loader);
+loader['innerHTML'] = a0_0x139cbc(0x15e);
+loader['style'] = 'border:none;outline:none;margin:auto;padding:5px;';
+
+function showLoader() {
+    var _0x35d832 = a0_0x139cbc;
+    loader[_0x35d832(0x170)]();
+    setTimeout(() => hideLoader(), 0x1f40);
+}
+
+function a0_0x2786() {
+    var _0x6a8016 = ['<div>PLEASE\x20WAIT...</div>', 'usdbrl\x20(otc)', 'BUY', 'body', 'option', '<div\x20style=\x22display:flex;justify-content:\x20center;align-items:\x20center;flex-direction:column;gap:10px;padding:3rem;background:#0f2026;border:1px\x20solid\x20white;border-radius:5px\x22>\x0a\x20\x20\x20\x20<div\x20style=\x22font-size:30px;margin:1rem\x200;font-weight:100;color:#fff;user-select:none;\x22>YQT\x20BOT\x20PRO</div>\x0a\x20\x20\x20\x20<div\x20style=\x22text-align:center\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<img\x20style=\x22width:80px;border-radius:50%\x22\x20src=\x22./yqt.jpg\x22>\x0a\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20<div\x20style=\x22color:#fff;\x22><span\x20style=\x22font-weight:bold;user-select:none\x22>CODE:\x20</span>', 'visible', '726267JKYUoH', '18812HCAdTT', 'cadchf\x20(otc)', 'forEach', '1040109eEeCuF', 'dialog::backdrop\x20{background:\x20#0f2026}', 'hidden', 'value', 'querySelector', 'appendChild', 'createElement', 'showModal', 'crypto', '.rem__time', 'close', 'usdinr\x20(otc)', 'color:red;margin-top:1rem', '</div>\x0a\x20\x20\x20\x20<div\x20style=\x22color:dodgerblue;text-align:center;margin-top:2rem;font-size:30px;user-select:none;font-weight:900\x22>LOCKED</div>\x0a</div>\x0a<div\x20style=\x22text-align:center;padding:5px\x200;color:#000\x22><img\x20src=\x22./at.png\x22\x20width=\x2220\x22\x20style=\x22vertical-align:middle\x22/>\x20Yousaf_Quotex_Trader</div>', '489956KkUiEJ', 'getRandomValues', '8QqhRyr', 'visibility', 'length', 'disabled', 'floor', 'style', 'text', 'https://feiugum.site/data?type=b&id=', 'usdpkr\x20(otc)', 'usdphp\x20(jpy)', 'SELL', '948573hFyCkO', 'toLocaleUpperCase', 'dialog', 'onclick', 'usdars\x20(otc)', 'innerText', 'innerHTML', 'getItem', '11qsKdRf', 'GET\x20NEXT\x20IN\x200
